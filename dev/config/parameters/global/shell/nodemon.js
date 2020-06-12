d1e7cc49fdd8339f8eb9bf04291589620c727a73
Julian Wolfe <juliancwolfe@gmail.com>
2020-06-12 03:04:14 -0400

    Adds hooks on stdout, stderr of child processes

--- dev/config/parameters/global/shell/nodemon.js
+++ dev/config/parameters/global/shell/nodemon.js
@@ -1,9 +1,20 @@
 import path from 'path';
+import {terminal} from 'tramway-command';
+const {
+    TimestampLog,
+    TimestampError, 
+} = terminal;
 
 export default {
     command: path.resolve('./node_modules/.bin/nodemon'), 
     spawnOptions: {
-        stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
+        stdio: ['pipe', 'pipe', 'pipe'],
         shell: 'win32' === process.platform,
     },
+    onMessageHook: message => {
+        new TimestampLog(message);
+    },
+    onErrorMessageHook: message => {
+        new TimestampError(message);
+    }
 }
